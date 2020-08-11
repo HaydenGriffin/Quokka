f160180d5fac9fd494d68c3a651cbcfeddae0ef6
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-11 17:29:01 +0100

    added tiles to home

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import GridTiles from './components/gridTemplate';
+import Projectbtn from './components/Button';
+import Title from './components/PageTitleHeader';
 
 export default function Home() {
   return (
-    <h1>This is the home page</h1>
-
+    <div className="pl-32">
+      <Title/>
+      <Projectbtn/>
+      <GridTiles/>
+    </div>
   );
 }
