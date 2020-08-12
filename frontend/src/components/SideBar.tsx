4d2940cb25ed11a60c50d65eba3fe57f7e861e3d
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-12 11:27:45 +0100

    changed function component syntax

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {ReactComponent as HomeIcon} from '../assets/icons/home.svg';
 import {ReactComponent as SetlistIcon} from '../assets/icons/list.svg';
 import {ReactComponent as TrackIcon} from '../assets/icons/record-thicc.svg';
@@ -7,17 +7,19 @@ import {ReactComponent as ArtistIcon} from '../assets/icons/mic-thicc.svg';
 
 const IconClassName = 'icon';
 
-export default class Sidebar extends Component {
-    render() {
-        return (
-            <div className="sidebar">
-                <HomeIcon className={IconClassName}/>
-                <ArtistIcon className={IconClassName}></ArtistIcon>
-                <TrackIcon className={IconClassName}></TrackIcon>
-                <SetlistIcon className={IconClassName}/>
-               
-            </div>
+const Sidebar: React.FC = () => {
+    return (
+        <div className="sidebar">
+            <HomeIcon className={IconClassName}/>
+            <ArtistIcon className={IconClassName}></ArtistIcon>
+            <TrackIcon className={IconClassName}></TrackIcon>
+            <SetlistIcon className={IconClassName}/>
+        </div>
+    
+    )
 
-        )
-    }
 }
+export default Sidebar
+    
+
+
