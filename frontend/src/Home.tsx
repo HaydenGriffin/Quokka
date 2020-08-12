cc1b64b526680f41542738c71695cabe2ae032c4
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-12 15:14:47 +0100

    removed gridTemplate

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import GridTiles from './components/gridTemplate';
 import Projectbtn from './components/Button';
 import Title from './components/PageTitleHeader';
+import Tile from './components/ProjectTile';
 
 export default function Home() {
   return (
     <div className="pl-32">
       <Title/>
       <Projectbtn/>
-      <GridTiles/>
+      <div className="grid grid-cols-6 pt-32 col-gap-2 row-gap-12">
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      <Tile/>
+      </div>
     </div>
   );
 }
