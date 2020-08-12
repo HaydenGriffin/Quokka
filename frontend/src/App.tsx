ee5645a5f820267efb9a1c065448979d2318dde2
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-12 12:35:42 +0100

    fixed linter quotes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SideBar from "./components/SideBar";
-import Home from "./Home";
-import "./styles/main.css";
-import TopBar from "./components/TopBar";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import Home from './Home';
+import './styles/main.css';
+import TopBar from './components/TopBar';
 
 export default function App() {
   return (
