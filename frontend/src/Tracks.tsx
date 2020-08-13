c26727eff0bec92113669b32b2aeeb3bcd241c20
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-13 12:52:07 +0100

    edited tracks page

--- frontend/src/Tracks.tsx
+++ frontend/src/Tracks.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
 import Button from './components/Button';
 import Title from './components/PageTitleHeader';
-import Tile from './components/ProjectTile';
+import TrackTile from './components/TracksTile';
+import { ReactComponent as TracksIcon } from './assets/icons/record-thicc.svg';
 
-export default function Home() {
+export default function Tracks() {
   return (
     <div className="pl-32">
       <Title title="Tracks">
+        <TracksIcon className="w-12 h-12 mr-5" />
         <Button>Create Track</Button>
       </Title>
       <div className="grid grid-cols-6 pt-32 col-gap-2 row-gap-12">
-        <Tile
+        <TrackTile
           notification={true}
-          artist="Ed Sheeran"
-          set="Live Set"
-          sets={66}
-          tracks={24}
-          users={3}
+          trackname="Sing"
+          sections={4}
+          layers={12}
+          media={3}
+          notch={4}
+          imag={2}
+          assigned={1}
         />
-        <Tile
+        <TrackTile
           notification={false}
-          artist="Taylor swift"
-          set="Redemption"
-          sets={4}
-          tracks={32}
-          users={6}
+          trackname="Bloodstream"
+          sections={4}
+          layers={12}
+          media={3}
+          notch={4}
+          imag={2}
+          assigned={1}
         />
       </div>
     </div>
