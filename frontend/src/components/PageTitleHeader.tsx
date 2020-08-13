5e544585279b86e206fa556dcf1f7a84d41dc82e
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-13 12:51:40 +0100

    removed artist icon

--- frontend/src/components/PageTitleHeader.tsx
+++ frontend/src/components/PageTitleHeader.tsx
@@ -1,5 +1,4 @@
 import React, { PropsWithChildren, FC } from 'react';
-import { ReactComponent as ArtistIcon } from '../assets/icons/mic-thicc.svg';
 
 type PageTitleProps = {
   title: string;
@@ -11,7 +10,6 @@ const PageTitleHeader: FC<PropsWithChildren<PageTitleProps>> = ({
 }: PropsWithChildren<PageTitleProps>) => {
   return (
     <div className="flex mt-10 items-center">
-      <ArtistIcon className="w-12 h-12" />
       <h1 className="text-6xl font-bold mr-10">{title}</h1>
       {children}
     </div>
