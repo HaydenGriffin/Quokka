cb6aaa5464e61be0e6375ea87bf4c9a396dbac2f
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-16 20:34:12 +0100

    added placeholder to input

--- frontend/src/components/ProjectModal.tsx
+++ frontend/src/components/ProjectModal.tsx
@@ -7,9 +7,12 @@ const ProjectModal: FC = () => {
     <div className="w-full h-full absolute flex justify-center items-center inset-0 bg-black bg-opacity-25 z-20 ">
       <div className="w-auto h-64 bg-white relative p-10 py-5">
         <CloseIco className="w-6 absolute right-0 mt-0 mr-5" />
-        <h1 className="text-3xl font-medium mt-3">New Artist</h1>
-        <p className="subtitle mb-3">Artistname</p>
-        <input type="text" className="bg-primary py-2 px-1 w-full mb-6" />
+        <h1 className="text-3xl font-medium mt-3 mb-3">New Artist</h1>
+        <input
+          type="text"
+          placeholder="Artistname"
+          className="bg-primary py-2 px-1 w-full mb-6"
+        />
         <Button>Create Artist</Button>
       </div>
     </div>
