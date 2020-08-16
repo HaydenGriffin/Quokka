53191cddeaa062842672b6b11b3470c2d49fa0d9
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-16 14:34:59 +0100

    changed z-index

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -8,7 +8,7 @@ const IconClassName = 'icon';
 
 const Sidebar: FC = () => {
   return (
-    <div className="w-16 pt-24 fixed h-screen bg-accent">
+    <div className="w-16 pt-24 fixed h-screen bg-accent z-10">
       <HomeIcon className={IconClassName} />
       <a href="/">
         <ArtistIcon className="icon-active" />
