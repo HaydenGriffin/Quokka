4125239d22a44cea016ad2d192abab72af2f058e
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-19 21:50:00 +0100

    added setlistheader and setlisttiles

--- frontend/src/setlist.tsx
+++ frontend/src/setlist.tsx
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './components/Button';
 import Title from './components/PageTitleHeader';
 import SetlistTile from './components/SetlistTile';
 import { ReactComponent as SetlistIcon } from './assets/icons/list.svg';
-import ArtistHeader from './components/ArtistHeader';
+import SetlistHeader from './components/SetlistHeader';
+import SetlistDialog from './components/SetlistDialog';
 
 export default function Setlist() {
+  const [showProjectDialog, setShowDialog] = useState(false);
+  const openProjectDialog = () => setShowDialog(true);
+
   return (
-    <div>
-      <ArtistHeader
-        title="Ed Sheeran"
-        setlist="Divide 2019"
-        pagename="Tracks"
-        pageinfo="This is where you can create setlists"
-      />
-      <div className="pl-32">
-        <Title title="Tracks">
-          <SetlistIcon className="w-12 h-12 mr-12" />
-          <Button>Create Setlist</Button>
-        </Title>
-        <div className="grid grid-cols-6 pt-16 col-gap-2 row-gap-12">
-          <SetlistTile
-            notification={true}
-            setname="Divide 2019"
-            version={4}
-            dateModified="24 July 2020"
-            tracks={24}
-            users={3}
-          />
-          <SetlistTile
-            notification={false}
-            setname="Glastonbury 2017"
-            version={4}
-            dateModified="15th April 2017"
-            tracks={24}
-            users={3}
-          />
+    <>
+      <SetlistDialog isOpen={showProjectDialog} setShowDialog={setShowDialog} />
+      <div>
+        <SetlistHeader title="Ed Sheeran" setlist="Divide 2019" />
+        <div className="pl-32">
+          <Title title="Setlist">
+            <SetlistIcon className="w-12 h-12 mr-12" />
+            <Button onClick={openProjectDialog}>Create Setlist</Button>
+          </Title>
+          <div className="grid grid-cols-6 pt-16 col-gap-2 row-gap-12">
+            <SetlistTile
+              notification={true}
+              setname="Divide 2019"
+              version={4}
+              dateModified="24 July 2020"
+              tracks={24}
+              users={3}
+            />
+            <SetlistTile
+              notification={false}
+              setname="Glasto 2017"
+              version={4}
+              dateModified="15th April 2017"
+              tracks={24}
+              users={3}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
