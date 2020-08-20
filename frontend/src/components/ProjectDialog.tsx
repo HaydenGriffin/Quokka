8ee83429a251847a8d711e679324b16a19efe200
Matt Cromwell <mattcromwell2@gmail.com>
2020-08-20 10:02:32 +0100

    hook for input value, started function for new artist

--- frontend/src/components/ProjectDialog.tsx
+++ frontend/src/components/ProjectDialog.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { ReactComponent as CloseIco } from '../assets/icons/x.svg';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 import Button from './Button';
@@ -13,23 +13,37 @@ const ProjectDialog: FC<ProjectDialogProps> = ({
   setShowDialog,
 }: ProjectDialogProps) => {
   const closeProjectDialog = () => setShowDialog(false);
+
+  // hook for getting input value
+  const [inputValue, setInputValue] = useState<string>('');
+  // function for new artist POST
+  const NewArtist = () => {
+    console.log(inputValue);
+  };
+
   return (
     <DialogOverlay
       isOpen={isOpen}
       onDismiss={closeProjectDialog}
       className="fixed bg-black bg-opacity-25 inset-0 z-20 "
     >
-      <DialogContent className="relative w-64 bg-white">
+      <DialogContent className="w-1/4 bg-white relative mx-auto my-32 px-8 py-5">
         <button onClick={closeProjectDialog}>
           <CloseIco className="w-6 absolute right-0 mt-0 mr-5" />
         </button>
         <h1 className="text-3xl font-medium mt-3 mb-3">New Artist</h1>
-        <input
-          type="text"
-          placeholder="Artist Name"
-          className="bg-primary py-2 px-1 w-full mb-6"
-        />
-        <Button>Create Artist</Button>
+        <form>
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="Artist Name"
+            className="bg-primary py-2 px-1 w-full mb-6"
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>): void =>
+              setInputValue(ev.target.value)
+            }
+          />
+        </form>
+        <Button onClick={NewArtist}>Create Artist</Button>
       </DialogContent>
     </DialogOverlay>
   );
