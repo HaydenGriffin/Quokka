1232e6224e7007ca9bf4c29408dae5420a150431
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-04 22:25:20 +0100

    changed baseUrl

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import { ArtistAPI } from './artist/artist';
 import { TourAPI } from './tour/tour';
 import { TourMemberAPI } from './tourMember/tourMember';
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl = 'https://casbp529v8.execute-api.eu-west-2.amazonaws.com/dev';
 
 const artist = new ArtistAPI(baseUrl);
 const tour = new TourAPI(baseUrl);
