41f51b5918190186ce75e3bd0812d2980e0bfa3a
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-04 20:13:05 +0100

    styling on image

--- frontend/src/Landing.tsx
+++ frontend/src/Landing.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SignupButton from './components/SignupButton';
 import EdLanding from './assets/images/Ed-Landing.jpg';
+import LoginButton from './components/LoginButton';
 
 export default function Landing() {
   return (
     <>
       <div className="w-screen h-screen absolute bg-darkgrey z-20 flex">
-        <div className="pl-16 pt-48 w-2/3">
+        <div className="px-16 pt-48 w-1/2">
           <h1 className="text-white font-medium m-0 p-0 text-3xl">Quokka</h1>
           <h1 className="text-white font-black text-6xl uppercase m-0">
             Tour Workflow
@@ -16,13 +17,11 @@ export default function Landing() {
           </p>
           <SignupButton />
           <p className="text-white">
-            Already have an account?{' '}
-            <a href="#" className="underline">
-              Log in
-            </a>
+            Already have an account?
+            <LoginButton />
           </p>
         </div>
-        <img src={EdLanding} alt="" className="w-2/3 h-auto" />
+        <img src={EdLanding} alt="" className="w-1/2" />
       </div>
     </>
   );
