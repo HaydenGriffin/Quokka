a3093d1ecf957b5b2471eaf7935176e58a834b68
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-05 19:24:12 +0100

    removed <>

--- frontend/src/components/ActivityItem.tsx
+++ frontend/src/components/ActivityItem.tsx
@@ -10,15 +10,13 @@ const ActivityItem: FC<ActivityItemProps> = ({
   message,
 }: ActivityItemProps) => {
   return (
-    <>
-      <div className="relative flex text-left items-center pl-3">
-        <div className="w-12 h-12 bg-accent rounded-full cursor-pointer"></div>
-        <div className="bg-white m-6 py-3">
-          <h1>{user}</h1>
-          <p className="font-medium text-xl">{message}</p>
-        </div>
+    <div className="relative flex text-left items-center pl-3">
+      <div className="w-12 h-12 bg-accent rounded-full cursor-pointer"></div>
+      <div className="bg-white m-6 py-3">
+        <h1>{user}</h1>
+        <p className="font-medium text-xl">{message}</p>
       </div>
-    </>
+    </div>
   );
 };
 export default ActivityItem;
