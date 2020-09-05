a88bf4cedd3cae783810dfe88464297c86b6dcfa
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-05 18:09:48 +0100

    included dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,15 @@ import Tracks from './Tracks';
 import Setlist from './setlist';
 import './styles/main.css';
 import TopBar from './components/TopBar';
-import ActivityPanel from './components/ActivityPanel';
+import Dashboard from './dashboard';
 
 export default function App() {
   return (
     <Router>
-      <ActivityPanel />
       <SideBar />
       <TopBar />
       <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+        <Route exact path="/" component={Dashboard}></Route>
         <Route path="/tracks">
           <Tracks />
         </Route>
