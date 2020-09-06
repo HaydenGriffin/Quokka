b5c9b55000aacac548b2294222816f7b31d3905d
Matt Cromwell <66025680+mattcrom15@users.noreply.github.com>
2020-09-06 13:20:34 +0100

    Dashboard (#12)
    * Create dashboard.tsx and DashboardTiles

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -3,13 +3,14 @@ import { ReactComponent as HomeIcon } from '../assets/icons/home.svg';
 import { ReactComponent as SetlistIcon } from '../assets/icons/list.svg';
 import { ReactComponent as TrackIcon } from '../assets/icons/record-thicc.svg';
 import { ReactComponent as ArtistIcon } from '../assets/icons/mic-thicc.svg';
-
+import { ReactComponent as TourIcon } from '../assets/icons/globe.svg';
 const IconClassName = 'icon';
 
 const Sidebar: FC = () => {
   return (
     <div className="w-16 pt-24 fixed h-screen bg-accent z-10">
       <HomeIcon className={IconClassName} />
+      <TourIcon className={IconClassName} />
       <a href="/">
         <ArtistIcon className="icon-active" />
       </a>
