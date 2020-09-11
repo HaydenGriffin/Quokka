b50bb4e39ad3b730ef3d8660faf31617ccb610c5
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-11 18:16:41 +0100

    added user name

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import DashboardTile from './components/DashboardTile';
 import ActivityPanel from './components/ActivityPanel';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function Home() {
+  const { user } = useAuth0();
+  const { name } = user;
   return (
     <>
       <div className="flex">
         <div className="pl-32 mt-10 w-full">
-          <h1 className="text-5xl font-bold mb-5">Hi Matt!</h1>
+          <h1 className="text-5xl font-bold mb-5">Hi {name}!</h1>
           <p className="text-xl font-regular mb-5">
             Your recent tours{'  '}
             <a href="#" className="underline text-gray-600">
