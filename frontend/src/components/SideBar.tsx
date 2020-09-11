9e9a5b5e027b9c4767c29478b4f40e4804461661
Matt Cromwell <mattcromwell2@gmail.com>
2020-09-11 19:45:36 +0100

    removed track and setlist icon

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
 import { ReactComponent as HomeIcon } from '../assets/icons/home.svg';
-import { ReactComponent as SetlistIcon } from '../assets/icons/list.svg';
-import { ReactComponent as TrackIcon } from '../assets/icons/record-thicc.svg';
 import { ReactComponent as ArtistIcon } from '../assets/icons/mic-thicc.svg';
 import { ReactComponent as TourIcon } from '../assets/icons/globe.svg';
 const IconClassName = 'icon';
@@ -12,14 +10,10 @@ const Sidebar: FC = () => {
       <a href="/">
         <HomeIcon className={IconClassName} />
       </a>
-      <TourIcon className={IconClassName} />
       <a href="/artist">
         <ArtistIcon className="icon-active" />
       </a>
-      <a href="/tracks">
-        <TrackIcon className={IconClassName} />
-      </a>
-      <SetlistIcon className={IconClassName} />
+      <TourIcon className={IconClassName} />
     </div>
   );
 };
