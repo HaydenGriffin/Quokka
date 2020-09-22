a615e75bf01f34bd947d3d174fab44c56404c69a
Hayden Griffin <haydengriffin@hotmail.co.uk>
2020-09-22 16:26:56 +0100

    Replaced <a href> links with react-router-dom <NavLink>

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,17 +1,27 @@
 import React, { FC } from 'react';
 import { HomeIcon, ArtistIcon, TourIcon } from '../assets/icons/index';
+import { NavLink } from 'react-router-dom';
 const IconClassName = 'icon';
 
 const Sidebar: FC = () => {
   return (
     <div className="w-16 pt-24 fixed h-screen bg-accent z-10">
-      <a href="/">
-        <HomeIcon className={IconClassName} />
-      </a>
-      <a href="/artist">
-        <ArtistIcon className="icon-active" />
-      </a>
-      <TourIcon className={IconClassName} />
+      <NavLink
+        exact
+        to="/"
+        className={IconClassName}
+        activeClassName="icon-active"
+      >
+        <HomeIcon />
+      </NavLink>
+      <NavLink
+        to="/artist"
+        className={IconClassName}
+        activeClassName="icon-active"
+      >
+        <ArtistIcon />
+      </NavLink>
+      <TourIcon />
     </div>
   );
 };
